72619c6e0944608ff1c80aca904c0874b1dd17d3
Ahmed ZEKRI <az@vyzyo.com>
2022-02-14 17:15:31 +0100

    Beginning of viewModel

---
+++ app/src/main/java/com/example/android_mvvm_best_pratices/ui/component/login/LoginViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.android_mvvm_best_pratices.ui.component.login
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor() {
+}
