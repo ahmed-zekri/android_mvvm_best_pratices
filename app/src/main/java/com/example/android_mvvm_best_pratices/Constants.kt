70b7cb6b840db4ff82e7961fa5e5a0bcca68c760
Ahmed ZEKRI <az@vyzyo.com>
2022-02-17 09:43:11 +0100

    Updated baseurl

--- app/src/main/java/com/example/android_mvvm_best_pratices/Constants.kt
+++ app/src/main/java/com/example/android_mvvm_best_pratices/Constants.kt
@@ -1,4 +1,4 @@
 package com.example.android_mvvm_best_pratices
 
 const val SPLASH_DELAY = 3000
-const val BASE_URL = "http://192.168.43.213:8080/api/"
+const val BASE_URL = "https://social-media-spring-boot-back.herokuapp.com/api/"
