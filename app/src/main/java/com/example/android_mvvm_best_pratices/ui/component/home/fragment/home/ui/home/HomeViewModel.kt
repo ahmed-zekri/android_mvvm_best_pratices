e5835f99a9125503e8d3188d54a5167d96e27e01
ahmed103 <ahmed.zekri.a@gmail.com>
2022-02-22 00:12:03 +0100

    View model  get movies

--- app/src/main/java/com/example/android_mvvm_best_pratices/ui/component/home/fragment/home/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/android_mvvm_best_pratices/ui/component/home/fragment/home/ui/home/HomeViewModel.kt
@@ -1,19 +1,12 @@
 package com.example.android_mvvm_best_pratices.ui.component.home.fragment.home.ui.home
 
 import androidx.lifecycle.viewModelScope
-import com.bumptech.glide.load.engine.Resource
-import com.example.android_mvvm_best_pratices.data.dto.Movie
 import com.example.android_mvvm_best_pratices.data.repositories.movie.DataRepositoryMovieImpl
 import com.example.android_mvvm_best_pratices.ui.component.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
-import java.util.concurrent.Flow
-
 import javax.inject.Inject
-import kotlin.coroutines.CoroutineContext
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
