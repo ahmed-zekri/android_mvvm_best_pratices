8a539b05b47a4017cd43b1cf2dd6e56aa524c2f0
ahmed103 <ahmed.zekri.a@gmail.com>
2022-02-22 23:43:12 +0100

    Nullable items

--- app/src/main/java/com/example/android_mvvm_best_pratices/data/dto/Movie.kt
+++ app/src/main/java/com/example/android_mvvm_best_pratices/data/dto/Movie.kt
@@ -3,9 +3,9 @@ package com.example.android_mvvm_best_pratices.data.dto
 import com.example.android_mvvm_best_pratices.ui.component.base.AdapterListItem
 
 data class Movie(
-    override var id: Long? = null, var episodes: Int,
+    override var id: Long? = null, var episodes: Int?,
 
-    var title: String,
-    var rating: Float,
-    var isFavourite: Boolean = false
+    var title: String?,
+    var rating: Float?,
+    var isFavourite: Boolean? = false
 ) : AdapterListItem
--- app/src/main/java/com/example/android_mvvm_best_pratices/data/remote/ServiceGenerator.kt
+++ app/src/main/java/com/example/android_mvvm_best_pratices/data/remote/ServiceGenerator.kt
@@ -59,7 +59,7 @@ class ServiceGenerator @Inject constructor(
         okHttpBuilder.readTimeout(timeoutRead.toLong(), TimeUnit.SECONDS)
         val client = okHttpBuilder.build()
         retrofit = Retrofit.Builder()
-            .baseUrl(TEST_BASE_URL).client(client)
+            .baseUrl(BASE_URL).client(client)
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
     }
